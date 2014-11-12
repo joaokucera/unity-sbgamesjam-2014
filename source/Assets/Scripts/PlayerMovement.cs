8b75c3c441fda80f84125ca18251b5cf74ab2724
joaokucera <falleirok@gmail.com>
2014-11-12 18:44:21 -0200

    screen resolution
    screen resolution

---
+++ source/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region [ FIELDS ]
+
+    private Vector2 speed = new Vector2(10, 10);
+    private Vector2 movement;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 spriteSize;
+
+    #endregion
+
+    #region [ METHODS ]
+
+    void Start()
+    {
+        spriteRenderer = renderer as SpriteRenderer;
+        spriteSize = spriteRenderer.bounds.size;
+    }
+
+    void Update()
+    {
+        Vector2 currentPosition = transform.position;
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(
+            inputX * speed.x,
+            inputY * speed.y);
+
+        EnforceBounds();
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = movement;
+    }
+
+    private void EnforceBounds()
+    {
+        Rect borders = ScreenResolution.Instance.RiverBorders;
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, borders.xMin + spriteSize.x / 2, borders.xMax + spriteSize.x / 2),
+            Mathf.Clamp(transform.position.y, borders.yMin + spriteSize.y / 2, borders.yMax + spriteSize.y / 2),
+            transform.position.z
+        );
+    }
+
+    #endregion
+}
---
+++ source/Assets/Scripts/ScreenResolution.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenResolution : MonoBehaviour
+{
+    #region [ FIELDS ]
+
+    public static ScreenResolution Instance;
+
+    private Rect screenBorders;
+    private Rect riverBorders;
+
+    #endregion
+
+    #region [ PROPERTIES ]
+
+    public Rect RiverBorders
+    {
+        get { return riverBorders; }
+    }
+
+    #endregion
+
+    #region [ METHODS ]
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        screenBorders = new Rect
+        (
+            -(camera.orthographicSize * camera.aspect),
+            -camera.orthographicSize,
+            camera.orthographicSize * camera.aspect,
+            camera.orthographicSize
+        );
+
+        Transform riverTransform = GameObject.FindGameObjectWithTag("River").transform;
+        Vector2 riverPosition = riverTransform.position;
+        SpriteRenderer riverRenderer = riverTransform.renderer as SpriteRenderer;
+
+        riverBorders.xMin = riverPosition.x - riverRenderer.bounds.size.x / 2;
+        riverBorders.xMax = riverPosition.x - riverRenderer.bounds.size.x / 4;
+        riverBorders.yMin = riverPosition.y - riverRenderer.bounds.size.y / 2;
+        riverBorders.yMax = riverPosition.y + riverRenderer.bounds.size.y / 2;
+
+        // 96 px
+        // 160 px
+        // 32 px
+    }
+
+    #endregion
+}
